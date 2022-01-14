f27d582fda3161bea145e124adb05b957662bc80
albe2669 <albert@risenielsen.dk>
2022-01-14 11:35:33 +0100

    Enable virtual text for the lsp

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -5,7 +5,7 @@ local api = vim.api
 
 local border_opts = { border = "single", focusable = false, scope = "line" }
 
-vim.diagnostic.config({ virtual_text = false, float = border_opts })
+vim.diagnostic.config({ virtual_text = true, float = border_opts })
 
 lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, border_opts)
 lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, border_opts)
