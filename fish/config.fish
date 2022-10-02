57c46152f8f8a424f51e9fb3941e0ffaf01d0bb1
Albert Rise Nielsen <albert@risenielsen.dk>
2022-10-02 13:49:57 +0200

    Add nvim and kubectl paths

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,9 @@ fish_add_path /usr/local/texlive/2022/bin/x86_64-linux
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 
+fish_add_path $HOME/Documents/Installs/nvim
 fish_add_path $HOME/Documents/Installs/lazygit
+fish_add_path $HOME/Documents/Installs/kubectl
 fish_add_path $HOME/Documents/Installs/dagger/bin
 fish_add_path $HOME/Documents/Installs/google-cloud-sdk/bin
 fish_add_path $HOME/Documents/Installs/lsp/lua/bin
