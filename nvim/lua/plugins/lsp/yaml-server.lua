a6016c9d19b78e6108ccea2b7e3c85c663139255
Albert Rise Nielsen <albert@risenielsen.dk>
2022-11-22 15:06:14 +0100

    Add github actions to yamlls

--- nvim/lua/plugins/lsp/yaml-server.lua
+++ nvim/lua/plugins/lsp/yaml-server.lua
@@ -1,14 +1,16 @@
 local Y = {
   setup = function(on_attach)
     local lspconfig = require("lspconfig")
-    
+
     lspconfig['yamlls'].setup({
       on_attach = on_attach,
       capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
       settings = {
         yaml = {
           schemas = {
-            ["kubernetes"] = ".infrastructure/kubernetes/**/*.yaml"
+            ["kubernetes"] = ".infrastructure/kubernetes/**/*.yaml",
+            ["https://json.schemastore.org/github-action.json"] = ".github/actions/**/*.yml",
+            ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/**/*.yml",
           }
         }
       }
