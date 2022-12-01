7cf255ac55a199ab1fdbe1cd5327832651075985
Albert Rise Nielsen <albert@risenielsen.dk>
2022-12-01 15:55:07 +0100

    Initial i3 setup

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set GDK_SCALE 2
+set GDK_DPI_SCALE 0.5
+set QT_AUTO_SCREEN_SCALE_FACTOR 1
+
 # Path
 fish_add_path /usr/local/go/bin
 fish_add_path /usr/local/texlive/2022/bin/x86_64-linux
