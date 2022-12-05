ffd28d38f5f780d9c33f6abf037f7ad5d5a22675
Albert Rise Nielsen <albert@risenielsen.dk>
2022-12-05 13:05:33 +0100

    Add gopath to the path

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,15 @@ set GDK_SCALE 2
 set GDK_DPI_SCALE 0.5
 set QT_AUTO_SCREEN_SCALE_FACTOR 1
 
+# Go stuff
+set -x GOPATH $HOME/.local/go
+
 # Path
 fish_add_path /usr/local/go/bin
 fish_add_path /usr/local/texlive/2022/bin/x86_64-linux
 
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/go/bin
 fish_add_path $HOME/.cargo/bin
 
 fish_add_path $HOME/Documents/Installs/nvim
