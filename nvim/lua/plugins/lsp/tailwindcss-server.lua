0b5692aeb0306014f302f4d4821f359be43e6d42
Albert Rise Nielsen <albert@risenielsen.dk>
2023-02-05 20:39:24 +0100

    Add svelte and tailwind servers

---
+++ nvim/lua/plugins/lsp/tailwindcss-server.lua
@@ -0,0 +1,11 @@
+local T = {
+  setup = function(on_attach)
+    local lspconfig = require("lspconfig")
+
+    lspconfig["tailwindcss"].setup({
+      on_attach = on_attach,
+    })
+  end
+}
+
+return T
