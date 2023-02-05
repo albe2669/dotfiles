264930c0254a92b9ad161d646021dd692ce55754
Albert Rise Nielsen <albert@risenielsen.dk>
2023-02-05 20:39:34 +0100

    Formatting

--- nvim/lua/plugins/lsp/yaml-server.lua
+++ nvim/lua/plugins/lsp/yaml-server.lua
@@ -4,7 +4,7 @@ local Y = {
 
     lspconfig['yamlls'].setup({
       on_attach = on_attach,
-      capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
       settings = {
         yaml = {
           schemas = {
