f45d6cab3135741ed5e7f00040fdce7aa7cc072d
Albert Rise Nielsen <albert@risenielsen.dk>
2023-02-05 20:39:52 +0100

    Ensure LSP's are installed and register new ones

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -18,73 +18,75 @@ local formatting = function()
       local params = lsp.util.make_formatting_params()
       local result, err = client.request_sync("textDocument/formatting", params, 5000, bufnr)
       if err then
-          local err_msg = type(err) == "string" and err or err.message
-          vim.notify("global.lsp.formatting: " .. err_msg, vim.log.levels.WARN)
-          return
+        local err_msg = type(err) == "string" and err or err.message
+        vim.notify("global.lsp.formatting: " .. err_msg, vim.log.levels.WARN)
+        return
       end
 
       if result and result.result then
-          local offset_encoding = client.offset_encoding
-          lsp.util.apply_text_edits(result.result, bufnr, offset_encoding)
+        local offset_encoding = client.offset_encoding
+        lsp.util.apply_text_edits(result.result, bufnr, offset_encoding)
       end
     end
   end
 end
 
 global.lsp = {
-    border_opts = border_opts,
-    formatting = formatting,
+  border_opts = border_opts,
+  formatting = formatting,
 }
 
 local on_attach = function(client, bufnr)
-    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-    vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
-    vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
-    vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
-    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+  vim.cmd("command! LspDiagPrev lua vim.diagnostic.goto_prev()")
+  vim.cmd("command! LspDiagNext lua vim.diagnostic.goto_next()")
+  vim.cmd("command! LspDiagLine lua vim.diagnostic.open_float()")
+  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
 
-    u.buf_map(bufnr, "n", "gd", ":LspDef<CR>")
-    u.buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
-    u.buf_map(bufnr, "n", "gI", ":LspImplementation<CR>")
-    u.buf_map(bufnr, "n", "K", ":LspHover<CR>")
-    u.buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
-    u.buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
-    u.buf_map(bufnr, "n", "<Leader>lr", ":LspRename<CR>")
-    u.buf_map(bufnr, "n", "<Leader>la", ":LspCodeAction<CR>")
-    u.buf_map(bufnr, "n", "<Leader>ld", ":LspDiagLine<CR>")
-    u.buf_map(bufnr, "i", "<Leader>lh", "<cmd> LspSignatureHelp<CR>")
+  u.buf_map(bufnr, "n", "gd", ":LspDef<CR>")
+  u.buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
+  u.buf_map(bufnr, "n", "gI", ":LspImplementation<CR>")
+  u.buf_map(bufnr, "n", "K", ":LspHover<CR>")
+  u.buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
+  u.buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
+  u.buf_map(bufnr, "n", "<Leader>lr", ":LspRename<CR>")
+  u.buf_map(bufnr, "n", "<Leader>la", ":LspCodeAction<CR>")
+  u.buf_map(bufnr, "n", "<Leader>ld", ":LspDiagLine<CR>")
+  u.buf_map(bufnr, "i", "<Leader>lh", "<cmd> LspSignatureHelp<CR>")
 
-    if client.supports_method("textDocument/formatting") then
-      vim.cmd("autocmd BufWritePre <buffer> lua global.lsp.formatting()")
-    end
+  if client.supports_method("textDocument/formatting") then
+    vim.cmd("autocmd BufWritePre <buffer> lua global.lsp.formatting()")
+  end
 
-    if client.supports_method("textDocument/completion") then
-      vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
-      u.buf_map(bufnr, "i", "<C-Space>", "<C-x><C-o>")
-    end
+  if client.supports_method("textDocument/completion") then
+    vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+    u.buf_map(bufnr, "i", "<C-Space>", "<C-x><C-o>")
+  end
 end
 
-for _, config in ipairs({ 
-  "null-ls",
-  "rs-server",
-  "ts-server",
-  "eslint-server",
-  "yaml-server",
-  "lua-server",
+for _, config in ipairs({
   "bash-server",
-  "terraform-server",
-  "dagger-server",
   "c-server",
   "cmake-server",
-  "python-server",
+  "dagger-server",
+  "eslint-server",
   "go-server",
+  "lua-server",
+  "null-ls",
+  "python-server",
+  "rs-server",
+  "svelte-server",
+  "tailwindcss-server",
+  "terraform-server",
+  "ts-server",
+  "yaml-server",
 }) do
   require("plugins.lsp." .. config).setup(on_attach)
 end
@@ -92,6 +94,20 @@ end
 require("mason").setup({})
 require("mason-lspconfig").setup({
   automatic_installation = true,
+  ensure_installed = {
+    "bashls",
+    "clangd",
+    "cmake",
+    "eslint",
+    "gopls",
+    "pylsp",
+    "rust_analyzer",
+    "svelte",
+    "tailwindcss",
+    "terraformls",
+    "tsserver",
+    "yamlls",
+  }
 })
 
 -- Hide lspconfig messages
@@ -103,5 +119,3 @@ vim.notify = function(msg, ...)
 
   notify(msg, ...)
 end
-
-
