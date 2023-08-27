9db22361ee5eb72bf63a15861a8f2545b68dda5c
Albert Rise Nielsen <albert@risenielsen.dk>
2023-08-27 18:37:55 +0200

    Add git modules

---
+++ modules/git/flake-module.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    bfg-repo-cleaner
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "albe2669";
+    userEmail = "albert@risenielsen.dk";
+
+    extraConfig = {
+      pull.rebase = false;
+      credential.helper = "store";
+    };
+
+    lfs = {
+      enable = true;
+    };
+  };
+}
