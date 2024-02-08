8f5f6e57aee682ebe16cd84a657e2ce2d25e79c7
Albert Rise Nielsen <albert@risenielsen.dk>
2024-02-08 21:52:51 +0100

    Started polybar move

--- home/polybar/config/launch.sh
+++ home/polybar/config/launch.sh
@@ -5,10 +5,15 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar --config=~/.config/polybar/config.ini left 2>&1 | tee -a /tmp/poly_left.log & disown
-polybar --config=~/.config/polybar/config.ini mid 2>&1 | tee -a /tmp/poly_mid.log & disown
-polybar --config=~/.config/polybar/config.ini right 2>&1 | tee -a /tmp/poly_rigt.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar.log
+
+polybar
+
+polybar --config=~/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar_main.log &
+
+echo "Bars launched..." | tee -a /tmp/polybar.log
+
