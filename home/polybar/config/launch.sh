2b476414d70f8756280b76c8baa69b2fa12c0f5f
Albert Rise Nielsen <albert@risenielsen.dk>
2024-02-15 23:06:47 +0100

    Many polybar work fixes

--- home/polybar/config/launch.sh
+++ home/polybar/config/launch.sh
@@ -11,9 +11,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar.log
 
-polybar
 
-polybar --config=~/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar_main.log &
+# TODO: Use this instead
+# polybar --config=~/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar_main.log &
+
+polybar --config=$PWD/config.ini main &
 
 echo "Bars launched..." | tee -a /tmp/polybar.log
 
