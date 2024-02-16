33f87baef2406fee02927a38138233ac3ba0f813
Albert Rise Nielsen <albert@risenielsen.dk>
2024-02-16 19:07:02 +0100

    Add gauss to latex

--- modules/tex/flake-module.nix
+++ modules/tex/flake-module.nix
@@ -3,7 +3,7 @@
 let 
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-medium
-      tufte-latex hardwrap catchfile titlesec adjustbox enumitem;
+      tufte-latex hardwrap catchfile titlesec adjustbox enumitem gauss;
   });
 in {
   home.packages = with pkgs; [
