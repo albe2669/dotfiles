10b95f06a182e0c3f66c1269ac28abae74409009
Albert Rise Nielsen <albert@risenielsen.dk>
2024-03-05 21:57:28 +0100

    Build packages with pulseaudio support

--- modules/services/pulseaudio.nix
+++ modules/services/pulseaudio.nix
@@ -1,12 +1,15 @@
 {pkgs, username, ...}: {
   environment.systemPackages = with pkgs; [
     pulseaudio
+    pavucontrol
   ];
 
   hardware.pulseaudio = { 
     enable = true;
     support32Bit = true;
   };
+
+  nixpkgs.config.pulseaudio = true;
 
   users.users.${username}.extraGroups = [ "audio" ];
 }
