15f23d4d08168bfbd3a022a7d3282f7e10d0c09a
Albert Rise Nielsen <albert@risenielsen.dk>
2024-03-05 22:09:15 +0100

    Add docker compose

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -1,6 +1,10 @@
-{lib, ...}: {
+{lib, pkgs, ...}: {
   virtualisation.docker = {
     enable = lib.mkDefault true;
     enableOnBoot = lib.mkDefault true;
   };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
 }
