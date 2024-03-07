f739f6c27070604a196b964216c67ca20f6320c3
Albert Rise Nielsen <albert@risenielsen.dk>
2024-03-07 13:27:24 +0100

    Use core-laptop in skein

--- hosts/skein/os.nix
+++ hosts/skein/os.nix
@@ -1,10 +1,10 @@
 {
-  config,
-  pkgs,
   ...
 }: {
   imports = [
     ../../modules/core-desktop.nix
+    # Probably does nothing as it's a vm, but it tests if the installation is successful.
+    ../../modules/core-laptop.nix
     ../../modules/services/bluetooth.nix
 
     # Include the results of the hardware scan.
