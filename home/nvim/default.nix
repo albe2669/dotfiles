4bb0c02d129816cdfd4b0997dd6b8f068bc256ef
Albert Rise Nielsen <albert@risenielsen.dk>
2024-04-03 23:29:15 +0200

    Install vimplug

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,9 +1,11 @@
 {
   pkgs,
   config,
+	lib,
   ...
 }: {
   home.packages = with pkgs; [
+		curl # for vimplug
     neovim
     virtualenv
     xclip
@@ -11,6 +13,11 @@
     tree-sitter
     stdenv.cc
   ];
+
+	home.activation.vimplug = lib.hm.dag.entryAfter ["installPackages"] ''
+		PATH="${config.home.path}/bin:$PATH"
+		sh -c 'curl -fLo "''${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	'';
 
   xdg.configFile.nvim = {
     source = config.lib.file.mkOutOfStoreSymlink ./config;
