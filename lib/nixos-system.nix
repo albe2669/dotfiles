b02d0bb72b19a23ad04f1f316c22fc3e4b8c0244
Albert Rise Nielsen <albert@risenielsen.dk>
2024-04-04 11:17:01 +0200

    Remove another variable import

--- lib/nixos-system.nix
+++ lib/nixos-system.nix
@@ -34,7 +34,6 @@ in
           imports = [homeModules];
 
           _module.args = {
-            variables = import ../variables.nix;
             # theme = import ../colors.nix;
           };
         };
