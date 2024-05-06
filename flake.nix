237e575ba373e5989b55ccdfafd130592c8a7dc0
albe2669 <albert@risenielsen.dk>
2024-05-06 17:14:31 +0200

    Make home-manager follow nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nixos-generators = {
