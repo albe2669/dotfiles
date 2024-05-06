5cbcf9036d30502d6488fa2bf6256e6d5090e2a9
albe2669 <albert@risenielsen.dk>
2024-05-06 17:14:19 +0200

    Remove unnecessary vim-plug install from nvim

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -15,12 +15,6 @@
     stdenv.cc
   ];
 
-	# TODO: Ensure that this respects dryrun and stuff
-	home.activation.vimplug = lib.hm.dag.entryAfter ["installPackages"] ''
-		PATH="${config.home.path}/bin:$PATH"
-		sh -c 'curl -fLo "''${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	'';
-
   xdg.configFile.nvim = {
     source = config.lib.file.mkOutOfStoreSymlink "${variables.dotfilesLocation}" + (builtins.toPath "/home/nvim/config");
   };
