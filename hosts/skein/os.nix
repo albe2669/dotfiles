99626b67c87242fcb9717608e91aa30e1f4156eb
albe2669 <albert@risenielsen.dk>
2024-05-13 15:20:02 +0200

    Add virtualbox

--- hosts/skein/os.nix
+++ hosts/skein/os.nix
@@ -6,6 +6,7 @@ in {
     # Probably does nothing as it's a vm, but it tests if the installation is successful.
     ../../modules/core-laptop.nix
     ../../modules/services/bluetooth.nix
+    ../../modules/services/virtualbox.nix
 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
