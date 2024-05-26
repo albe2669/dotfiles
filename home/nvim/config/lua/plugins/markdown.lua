e86d3d224805eb9a5422383bf8c68d76963fd37d
albe2669 <albert@risenielsen.dk>
2024-05-26 11:15:21 +0200

    Add git restore . to the markdown-preview.nvim install command to avoid the local changes error

--- home/nvim/config/lua/plugins/markdown.lua
+++ home/nvim/config/lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
+    build = "cd app && npm install && git restore .",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
