15f6c5c09c7a4ee300a23ee162c9c85845c6a261
albe2669 <albert@risenielsen.dk>
2024-09-20 09:03:58 +0200

    kitty: update to v0.9.0

--- pkgs/kitty/default.nix
+++ pkgs/kitty/default.nix
@@ -4,30 +4,23 @@
 , libiconv
 , pkg-config
 , openssl
-# , docker
 }: 
 
 rustPlatform.buildRustPackage rec {
   pname = "kitty";
-  version = "v0.8.0";
+  version = "v0.9.0";
 
   src = fetchFromGitHub {
-    owner = "albe2669";
+    owner = "avborup";
     repo = pname;
-    rev = "nix";
-    sha256 = "sha256-D/9yp2n/Swoik6GUBUM71M5p45uqh67IvKDq/QQKA0I=";
+    rev = version;
+    sha256 = "sha256-6/ednV6hpTObID8VgSxu0xw23DI9Njvz1UuGVWrQH0g=";
   };
 
-  cargoSha256 = "sha256-i/B9p5O5Xm9p3QebwNBnDBQWWM9l++ejTaHo2v9MLa8=";
+  cargoSha256 = "sha256-S8e2dVf56WXJLPoU44iM64rvAOQCfr++DzfFxX6p3Fg=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ libiconv openssl ];
-  # checkInputs = [ libiconv openssl docker ];
-
-  # checkPhase = ''
-  #   export HOME=$(pwd)
-  #   make test
-  # '';
 
   doCheck = false;
 
@@ -38,7 +31,7 @@ rustPlatform.buildRustPackage rec {
 
   meta = with lib; {
     description = "Kitty is a CLI for interacting with Kattis that allows you to test and submit problems straight from your terminal.";
-    homepage = "https://github.com/albe2669/kitty";
+    homepage = "https://github.com/avborup/kitty";
     license = with licenses; [ mit ];
     maintainers = ["Adrian Borup" "Albert Rise Nielsen"];
     mainProgram = "kitty";
