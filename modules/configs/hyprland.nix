7b0b4e93b7231af93e85d6ea654c4b868ae3a02e
albe2669 <albert@risenielsen.dk>
2024-09-20 11:24:42 +0200

    hyprland: initial config

---
+++ modules/configs/hyprland.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  imports = [
+    ../services/sddm
+  ];
+
+  programs = {
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    hyprlock = {
+      enable = true;
+    };
+  };
+
+
+  environment.systemPackages = [
+    pkgs.kitty # required for the default Hyprland config 
+  ];
+}
