b4f1da05b70907a184e0126338f3d67a60b4de17
Albert Rise Nielsen <albert@risenielsen.dk>
2024-10-27 20:29:14 +0100

    fmt: run

--- home/phpstorm.nix
+++ home/phpstorm.nix
@@ -1,4 +1,8 @@
-{pkgs, pkgs-unstable, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   home.packages = with pkgs-unstable; [
     (jetbrains.plugins.addPlugins pkgs-unstable.jetbrains.phpstorm [
       "github-copilot"
--- home/spacedrive.nix
+++ home/spacedrive.nix
@@ -1,5 +1,4 @@
-{ pkgs-unstable, ... }:
-{
+{pkgs-unstable, ...}: {
   home.packages = with pkgs-unstable; [
     spacedrive
   ];
