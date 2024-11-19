5dd79451c2fd1eea9e48d8b8c73cc721ded818f9
albe2669 <albert@risenielsen.dk>
2024-11-19 10:22:04 +0100

    nvim: remove typescript tools deactivation

--- home/nvim/config/lua/plugins/lsp/lsp-utils.lua
+++ home/nvim/config/lua/plugins/lsp/lsp-utils.lua
@@ -9,7 +9,7 @@ function utils.formatting()
   local bufnr = api.nvim_get_current_buf()
   -- Loop all the clients and use their formatting
   for _, client in ipairs(lsp.get_clients()) do
-    if client.name == "null-ls" or client.name == "clangd" or client.name == "typescript-tools" then
+    if client.name == "null-ls" or client.name == "clangd" then
       return
     end
     if client.supports_method("textDocument/formatting") then
