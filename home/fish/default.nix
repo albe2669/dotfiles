43890195d7dd248a1dfae29602e3938d45008fc1
albe2669 <albert@risenielsen.dk>
2024-12-13 13:22:13 +0100

    fix(fish): dagger path

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -46,7 +46,7 @@
       fish_add_path $HOME/Documents/Installs/kubectl
       fish_add_path $HOME/Documents/Installs/kustomize
       fish_add_path $HOME/Documents/Installs/kind
-      fish_add_path $HOME/Documents/Installs/dagger/bin
+      fish_add_path $HOME/Documents/Installs/dagger
       fish_add_path $HOME/Documents/Installs/rust_analyzer
       fish_add_path $HOME/Documents/Installs/zotero
       fish_add_path $HOME/Documents/Installs/google-cloud-sdk/bin
