750fbfd1597fa2757c3e2528515b39102b6c2dbc
albe2669 <albert@risenielsen.dk>
2025-02-23 13:32:01 +0100

    feat(git): better config

--- home/git/default.nix
+++ home/git/default.nix
@@ -15,6 +15,34 @@
     extraConfig = {
       pull.rebase = false;
       credential.helper = "store";
+      init.defaultBranch = "master";
+
+      # https://blog.gitbutler.com/how-git-core-devs-configure-git/ 
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      help.autocorrect = "prompt";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      rebase = {
+       autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
     };
 
     lfs = {
