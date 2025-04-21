f40656dd7088ea3e6b475ee763316ccca3d5ee81
albe2669 <albert@risenielsen.dk>
2025-04-21 17:50:45 +0200

    feat(phpstorm): add ideavim config

--- home/phpstorm.nix
+++ home/phpstorm.nix
@@ -1,7 +1,37 @@
-{pkgs-unstable, ...}: {
+{variables, pkgs-unstable, ...}: {
   home.packages = with pkgs-unstable; [
     (jetbrains.plugins.addPlugins pkgs-unstable.jetbrains.phpstorm [
       "github-copilot"
     ])
   ];
+
+  home.file."${variables.homeDirectory.path}/.ideavimrc" = {
+    text = ''
+      set ideavimr""" Map leader to space ---------------------
+      let mapleader=" "
+
+      """ Plugins  --------------------------------
+      set surround
+      set multiple-cursors
+      set commentary
+      set argtextobj
+      set easymotion
+      set textobj-entire
+      set ReplaceWithRegister
+
+      """ Plugin settings -------------------------
+      let g:argtextobj_pairs="[:],(:),<:>"
+
+      """ Common settings -------------------------
+      set showmode
+      set so=5
+      set incsearch
+      set nu
+
+      """ Idea specific settings ------------------
+      set ideajoin
+      set ideastatusicon=gray
+      set idearefactormode=keep
+    '';
+  };
 }
