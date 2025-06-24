d31e28d0c99982de4b625de1327c5b2ced27c048
albe2669 <albert@risenielsen.dk>
2025-06-24 14:29:36 +0200

    fix(claude): update

--- overlays/claude.nix
+++ overlays/claude.nix
@@ -1,10 +1,10 @@
-final: prev: let version = "1.0.11"; in {
+final: prev: let version = "1.0.33"; in {
   claude-code = prev.claude-code.overrideAttrs (old: {
     inherit version;
 
     src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-      hash = "sha256-IXNBNjt4Sh5pR+Cz2uEZcCop9reAmQ7hObohtN0f3Ww=";
+      hash = "sha256-AH/ZokL0Ktsx18DrpUKgYrZKdBnKo29jntwXUWspH8w=";
     };
   });
 }
