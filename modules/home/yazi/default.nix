2891349ee4920738506e56c37a43edb68795fcff
albe2669 <albert@risenielsen.dk>
2025-08-26 22:32:11 +0200

    refactor(yazi): rewrite config into pure nix

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -4,18 +4,63 @@
   ...
 }: {
   home.packages = with pkgs-unstable; [
-    yazi
     exiftool
     mediainfo
     poppler_utils
     ueberzugpp
-    xdragon
+    dragon-drop
     wl-clipboard
   ];
 
-  xdg.configFile.yazi = {
-    source =
-      config.lib.file.mkOutOfStoreSymlink "${config.opts.variables.dotfilesLocation}"
-      + (builtins.toPath "/modules/home/yazi/config");
+  programs.yazi = {
+    enable = true;
+    package = pkgs-unstable.yazi;
+    enableFishIntegration = true;
+
+    plugins = {
+      system-clipboard = ./config/plugins/system-clipboard.yazi;
+    };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = ["<C-n"];
+          run = "shell --confirm 'dragon-drop -x -i -T -A \'$@\''";
+        }
+        {
+          on = ["<C-y>"];
+          run = "plugin system-clipboard";
+        }
+        {
+          on = ["z"];
+          run = "plugin zoxide";
+        }
+        {
+          on = ["Z"];
+          run = "plugin fzf";
+        }
+        {
+          on = ["<C-u>"];
+          run = "shell --confirm 'unzip $@'";
+          desc = "Unzip files";
+        }
+      ];
+    };
+
+    settings = {
+      opener = {
+        zen = [{ run = "zen-beta '$@'"; desc = "Zen"; }];
+      };
+
+      open.prepend_rules = [
+        { mime = "image/*"; use = ["open" "zen" "reveal"]; }
+        { mime = "application/pdf"; use = ["zen" "zathura" "open" "reveal"]; }
+      ];
+
+      preview = {
+        max_width = 1920;
+        max_height = 1080;
+      };
+    };
   };
 }
