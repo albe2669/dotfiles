bf643475a621eb026e6305894af2b03b256efb1c
albe2669 <albert@risenielsen.dk>
2025-09-29 11:10:01 +0200

    fix(java): disable google-java-format

--- modules/home/nvim/config/lua/plugins/lsp/java-server.lua
+++ modules/home/nvim/config/lua/plugins/lsp/java-server.lua
@@ -11,19 +11,19 @@ return {
     -- Custom on_attach function for Java that disables LSP formatting
     local java_on_attach = function(client, bufnr)
       -- Disable LSP formatting for Java files (we'll use google-java-format instead)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
-      
+      -- client.server_capabilities.documentFormattingProvider = false
+      -- client.server_capabilities.documentRangeFormattingProvider = false
+
       -- Call the standard on_attach function
       on_attach(client, bufnr)
-      
+
       -- Set up google-java-format on save for Java files
-      vim.cmd([[
-        augroup JavaFormat
-          autocmd! * <buffer>
-          autocmd BufWritePre <buffer> silent! !google-java-format --replace %
-        augroup END
-      ]])
+      -- vim.cmd([[
+      --   augroup JavaFormat
+      --     autocmd! * <buffer>
+      --     autocmd BufWritePre <buffer> silent! !google-java-format --replace %
+      --   augroup END
+      -- ]])
     end
 
     require("lspconfig")["jdtls"].setup({
