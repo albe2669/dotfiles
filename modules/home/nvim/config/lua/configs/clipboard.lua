cf90afda229463f793ac839a43a9d8de712c50c8
albe2669 <albert@risenielsen.dk>
2025-11-04 13:12:51 +0100

    fix(nvim): properly remove new lines when pasting

--- modules/home/nvim/config/lua/configs/clipboard.lua
+++ modules/home/nvim/config/lua/configs/clipboard.lua
@@ -1,12 +1,12 @@
 vim.g["clipboard"] = {
   name = "wl-copy",
   copy = {
-    ["+"] = { "wl-copy -n" },
-    ["*"] = { "wl-copy -n" }
+    ["+"] = { "wl-copy", "-n" },
+    ["*"] = { "wl-copy", "-n" }
   },
   paste = {
-    ["+"] = { "wl-paste -n" },
-    ["*"] = { "wl-paste -n" }
+    ["+"] = { "wl-paste", "-n" },
+    ["*"] = { "wl-paste", "-n" }
   },
   cache_enabled = 1
 }
