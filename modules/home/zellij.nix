b9c005904f71a15bd638aa88b9d3dc2d94e3a916
albe2669 <albert@risenielsen.dk>
2025-12-05 12:19:01 +0100

    fix(zellij): fix for nvim

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -1,4 +1,4 @@
-{ pkgs-unstable, ...}: {
+{pkgs-unstable, ...}: {
   # stylix.targets.zellij.enable = true;
 
   programs.zellij = {
@@ -7,6 +7,59 @@
     enableFishIntegration = true;
     settings = {
       theme = "everforest-dark";
+
+      keybinds = {
+        "normal clear-defaults=true" = {
+          "bind \"Ctrl f\"" = {
+            SwitchToMode = ["Tmux"];
+          };
+          "unbind \"Ctrl b\"" = {};
+        };
+      };
+      "tmux clear-defaults=true" = {
+        "bind \"Ctrl f\"" = {
+          Write = 2;
+          SwitchToMode = "Normal";
+        };
+        "bind \"Esc\"" = {
+          SwitchToMode = "Normal";
+        };
+        "bind \"g\"" = {
+          SwitchToMode = "Locked";
+        };
+        "bind \"p\"" = {
+          SwitchToMode = "Pane";
+        };
+        "bind \"t\"" = {
+          SwitchToMode = "Tab";
+        };
+        "bind \"n\"" = {
+          SwitchToMode = "Resize";
+        };
+        "bind \"h\"" = {
+          SwitchToMode = "Move";
+        };
+        "bind \"s\"" = {
+          SwitchToMode = "Scroll";
+        };
+        "bind \"o\"" = {
+          SwitchToMode = "Session";
+        };
+        "bind \"q\"" = {
+          Quit = {};
+        };
+
+        # bind "Ctrl f" { Write 2; SwitchToMode "Normal"; }
+        # bind "Esc" { SwitchToMode "Normal"; }
+        # bind "g" { SwitchToMode "Locked"; }
+        # bind "p" { SwitchToMode "Pane"; }
+        # bind "t" { SwitchToMode "Tab"; }
+        # bind "n" { SwitchToMode "Resize"; }
+        # bind "h" { SwitchToMode "Move"; }
+        # bind "s" { SwitchToMode "Scroll"; }
+        # bind "o" { SwitchToMode "Session"; }
+        # bind "q" { Quit; }
+      };
     };
   };
 }
