2723907dbacd1c51c5cce893b6742d90294d7a4f
albe2669 <albert@risenielsen.dk>
2026-02-01 23:35:17 +0100

    fix: don't use a mono font in kitty

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,15 +1,18 @@
-{...}: {
+{config, ...}: {
   stylix.targets.kitty = {
     enable = true;
     colors.enable = true;
-    fonts = {
-      enable = true;
-    };
+    fonts.enable = false;
     opacity.enable = true;
   };
 
   programs.kitty = {
     enable = true;
+
+    font = {
+      inherit (config.stylix.fonts.sansSerif) name package;
+      size = config.stylix.fonts.sizes.terminal;
+    };
 
     settings = {
       scrollback_lines = 5000;
