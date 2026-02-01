a965e56e7e1e9e8a1fdf43b8d094e39ddb2a4f20
albe2669 <albert@risenielsen.dk>
2026-02-01 16:34:11 +0100

    feat: update rio colors

--- modules/home/rio/default.nix
+++ modules/home/rio/default.nix
@@ -4,18 +4,6 @@
   ...
 }:
 with config.opts; {
-  stylix.targets.rio = {
-    enable = true;
-    colors = {
-      enable = true;
-      override = {
-        background = theme.colors.bg_dim;
-      };
-    };
-    opacity.enable = false;
-    fonts.enable = false;
-  };
-
   programs.rio = {
     enable = true;
 
@@ -30,6 +18,7 @@ with config.opts; {
       };
       fonts = {
         size = 14;
+        family = theme.font.family;
 
         regular = {
           family = theme.font.family;
@@ -79,6 +68,78 @@ with config.opts; {
         blur = true;
         # Note: Rio does not support window padding like Alacritty
         # Alacritty had padding x=7, y=7
+      };
+
+      colors = let 
+        fg = "#D3C6AA";
+        bg_dim = "1E2326";
+        bg0 = "#272E33"; 
+        bg_visual = "#4C3743";
+
+        black = "#374247";
+        blue = "#7FBBB3";
+        cyan = "#83C092";
+        green = "#A7C080";
+        magenta = "#D699B6";
+        red = "#E67E80";
+        tabs = "#4F5B58";
+        white = "#9DA9A0";
+        yellow = "#DBBC7F";
+      in {
+        background = bg_dim;
+        foreground = fg;
+
+        # Selection
+        selection-background = bg_visual;
+        selection-foreground = fg;
+
+        # Navigation
+        # tabs-active = "";
+        # tabs-active-foreground = "";
+        # tabs-active-highlight = "";
+        # bar = "";
+        # split = "";
+        cursor = fg; # Color of the cursor
+        # vi-cursor = bg0;
+
+        # Search
+        search-match-background = bg0;
+        search-match-foreground = green;
+        search-focused-match-background = bg0;
+        search-focused-match-foreground = red;
+
+        # Regular colors
+        black = black;
+        blue = blue;
+        cyan = cyan;
+        green =  green;
+        magenta = magenta;
+        red = red;
+        tabs = tabs;
+        white = white;
+        yellow = yellow;
+
+        # Dim colors
+        # dim-black = "";
+        # dim-blue = "";
+        # dim-cyan = "";
+        # dim-foreground = "";
+        # dim-green = "";
+        # dim-magenta = "";
+        # dim-red = "";
+        # dim-white = "#829181";
+        # dim-yellow = "";
+
+        # Light colors
+        light-black = "#414b50";
+        light-blue = "#3A94C5";
+        light-cyan = "#35A77C";
+        light-foreground = "#5C6A72";
+        light-green = "#8DA101";
+        light-magenta = "#DF69BA";
+        light-red = "#F85552";
+        light-white = "#FFFBEF";
+        light-yellow = "#F57D26";
       };
     };
   };
