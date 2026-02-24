dc2bc9f64d857bbebebe5e9b1e68802892d2e12b
albe2669 <albert@risenielsen.dk>
2026-02-24 13:15:16 +0100

    fix(kitty): set background blur

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,4 +1,9 @@
-{config, lib, pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   stylix.targets.kitty = {
     enable = true;
     colors.enable = true;
@@ -14,12 +19,14 @@
       size = config.stylix.fonts.sizes.terminal;
     };
 
-    settings = {
-      scrollback_lines = 5000;
-      cursor_shape = "block";
-    } // lib.optionalAttrs config.opts.variables.isDarwin {
-    background_blur = 32;
-    shell = "${pkgs.fish}/bin/fish";
-  };
+    settings =
+      {
+        scrollback_lines = 5000;
+        cursor_shape = "block";
+      }
+      // lib.optionalAttrs config.opts.variables.isDarwin {
+        background_blur = 48;
+        shell = "${pkgs.fish}/bin/fish";
+      };
   };
 }
