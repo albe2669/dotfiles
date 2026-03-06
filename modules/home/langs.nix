9c60bed4e74a10b595c331022f5c29c575e3df20
albe2669 <albert@risenielsen.dk>
2026-03-06 09:18:07 +0100

    style: fmt

--- modules/home/langs.nix
+++ modules/home/langs.nix
@@ -5,38 +5,40 @@
   config,
   ...
 }: {
-  home.packages = with pkgs; [
-    nodejs_22
+  home.packages = with pkgs;
+    [
+      nodejs_22
 
-    # Go
-    pkgs-unstable.go
-    pkgs-unstable.golangci-lint
+      # Go
+      pkgs-unstable.go
+      pkgs-unstable.golangci-lint
 
-    # Rust
-    pkgs-unstable.cargo
-    pkgs-unstable.rustc
-    pkgs-unstable.rustfmt
-    pkgs-unstable.clippy
-    openssl # openssl-sys
-    pkg-config # openssl-sys
+      # Rust
+      pkgs-unstable.cargo
+      pkgs-unstable.rustc
+      pkgs-unstable.rustfmt
+      pkgs-unstable.clippy
+      openssl # openssl-sys
+      pkg-config # openssl-sys
 
-    # Java, also required by Scala metals
-    jdk17
+      # Java, also required by Scala metals
+      jdk17
 
-    # clojure
-    clojure
-    leiningen
+      # clojure
+      clojure
+      leiningen
 
-    opentofu
+      opentofu
 
-    # AI
-    pkgs-unstable.gemini-cli
+      # AI
+      pkgs-unstable.gemini-cli
 
-    # Erlang
-    erlang_28
-  ] ++ lib.optionals config.opts.variables.isDarwin [
-    pkgs.apple-sdk
-  ];
+      # Erlang
+      erlang_28
+    ]
+    ++ lib.optionals config.opts.variables.isDarwin [
+      pkgs.apple-sdk
+    ];
 
   home.sessionVariables = lib.mkIf config.opts.variables.isDarwin {
     LIBRARY_PATH = lib.makeLibraryPath [
