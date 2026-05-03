b09644fc1cde15af9844381699e3a7f0de1d5f1a
albe2669 <albert@risenielsen.dk>
2026-05-03 23:07:53 +0200

    fix(yazi): set alias

--- modules/features/yazi/default.nix
+++ modules/features/yazi/default.nix
@@ -1,92 +1,101 @@
-{ config, ... }: {
-  flake.modules.homeManager.yazi = {
-    pkgs-unstable,
-    config,
-    lib,
-    ...
-  }: let
-    isDarwin = config.opts.variables.isDarwin;
-  in {
-    stylix.targets.yazi.enable = true;
+{ config, ... }:
+{
+  flake.modules.homeManager.yazi =
+    {
+      pkgs-unstable,
+      config,
+      lib,
+      ...
+    }:
+    let
+      isDarwin = config.opts.variables.isDarwin;
+    in
+    {
+      stylix.targets.yazi.enable = true;
 
-    home.packages = with pkgs-unstable;
-      [
-        exiftool
-        mediainfo
-        poppler-utils
-        ueberzugpp
-        ripdrag
-      ]
-      ++ lib.optionals (!isDarwin) [
-        wl-clipboard
-      ];
-
-    programs.yazi = {
-      enable = true;
-      package = pkgs-unstable.yazi;
-      enableFishIntegration = true;
-
-      keymap = {
-        mgr.prepend_keymap = [
-          {
-            on = "<C-n>";
-            run = "shell -- ${lib.getExe pkgs-unstable.ripdrag} --no-click --and-exit --icon-size 64 --target --all \"$@\"";
-          }
-          {
-            on = "z";
-            run = "plugin zoxide";
-          }
-          {
-            on = "Z";
-            run = "plugin fzf";
-          }
-          {
-            on = "<C-u>";
-            run = "shell --confirm 'unzip $@'";
-            desc = "Unzip files";
-          }
+      home.packages =
+        with pkgs-unstable;
+        [
+          exiftool
+          mediainfo
+          poppler-utils
+          ueberzugpp
+          ripdrag
+        ]
+        ++ lib.optionals (!isDarwin) [
+          wl-clipboard
         ];
-      };
 
-      settings = {
-        opener = {
-          zen = [
+      programs.yazi = {
+        enable = true;
+        package = pkgs-unstable.yazi;
+        enableFishIntegration = true;
+
+        shellWrapperName = "y";
+
+        keymap = {
+          mgr.prepend_keymap = [
             {
-              run = "zen-beta \"$@\"";
-              desc = "Zen";
+              on = "<C-n>";
+              run = "shell -- ${lib.getExe pkgs-unstable.ripdrag} --no-click --and-exit --icon-size 64 --target --all \"$@\"";
+            }
+            {
+              on = "z";
+              run = "plugin zoxide";
+            }
+            {
+              on = "Z";
+              run = "plugin fzf";
+            }
+            {
+              on = "<C-u>";
+              run = "shell --confirm 'unzip $@'";
+              desc = "Unzip files";
             }
           ];
         };
 
-        open.prepend_rules = [
-          {
-            mime = "image/*";
-            use = [
-              "open"
-              "zen"
-              "reveal"
+        settings = {
+          opener = {
+            zen = [
+              {
+                run = "zen-beta \"$@\"";
+                desc = "Zen";
+              }
             ];
-          }
-          {
-            mime = "application/pdf";
-            use = [
-              "zen"
-              "zathura"
-              "open"
-              "reveal"
-            ];
-          }
-        ];
+          };
 
-        preview = {
-          max_width = 1920;
-          max_height = 1080;
+          open.prepend_rules = [
+            {
+              mime = "image/*";
+              use = [
+                "open"
+                "zen"
+                "reveal"
+              ];
+            }
+            {
+              mime = "application/pdf";
+              use = [
+                "zen"
+                "zathura"
+                "open"
+                "reveal"
+              ];
+            }
+          ];
+
+          preview = {
+            max_width = 1920;
+            max_height = 1080;
+          };
         };
       };
     };
-  };
 
-  flake.modules.combined.yazi = { ... }: {
-    hm.imports = [ config.flake.modules.homeManager.yazi ];
-  };
+  flake.modules.combined.yazi =
+    { ... }:
+    {
+      hm.imports = [ config.flake.modules.homeManager.yazi ];
+    };
 }
