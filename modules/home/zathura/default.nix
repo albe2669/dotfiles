32a90ea315debaeb8e6d6643c9f6f4e223721644
albe2669 <albert@risenielsen.dk>
2026-05-05 09:37:41 +0200

    fix(zathura): only install on linux

--- modules/home/zathura/default.nix
+++ modules/home/zathura/default.nix
@@ -1,13 +1,14 @@
 {
   pkgs,
+  lib,
   config,
   ...
 }: {
-  home.packages = with pkgs; [
+  home.packages = lib.mkIf pkgs.stdenv.isLinux (with pkgs; [
     zathura
-  ];
+  ]);
 
-  xdg.configFile.zathura = {
+  xdg.configFile.zathura = lib.mkIf pkgs.stdenv.isLinux {
     source = config.lib.file.mkOutOfStoreSymlink "${config.opts.variables.dotfilesLocation}" + (builtins.toPath "/modules/home/zathura/config");
   };
 }
