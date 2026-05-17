64cef4a5c2319020881a3565eaa324e7bda96f36
albe2669 <albert@risenielsen.dk>
2026-05-17 16:24:55 +0200

    feat(1password): add

--- modules/features/1password.nix
+++ modules/features/1password.nix
@@ -1,5 +1,9 @@
-{ config, ... }: {
-  flake.modules.nixos."1password" = { pkgs-unstable, username, ... }: {
+{config, ...}: {
+  flake.modules.nixos."1password" = {
+    pkgs-unstable,
+    username,
+    ...
+  }: {
     programs._1password = {
       enable = true;
       # pkg = pkgs-unstable._1password;
@@ -13,7 +17,21 @@
     };
   };
 
-  flake.modules.combined."1password" = { ... }: {
-    imports = [ config.flake.modules.nixos."1password" ];
+  flake.modules.darwin."1password" = {...}: {
+    homebrew.casks = [
+      "1password-cli"
+    ];
+  };
+
+  flake.modules.combined."1password" = {system, ...}: let
+    isDarwin = builtins.match ".*-darwin" system != null;
+  in {
+    imports = [
+      (
+        if isDarwin
+        then config.flake.modules.darwin."1password"
+        else config.flake.modules.nixos."1password"
+      )
+    ];
   };
 }
