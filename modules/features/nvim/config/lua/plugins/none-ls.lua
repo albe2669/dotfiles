b9b453e8dd20725af1615a42c396c2b60763643e
albe2669 <albert@risenielsen.dk>
2026-07-21 09:00:37 +0200

    fix(none-ls): disable

--- modules/features/nvim/config/lua/plugins/none-ls.lua
+++ modules/features/nvim/config/lua/plugins/none-ls.lua
@@ -6,24 +6,24 @@ return {
       local fmt = nls.builtins.formatting
       local dgn = nls.builtins.diagnostics
       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      nls.setup({
-        sources = {
-          -- # FORMATTING #
-          fmt.google_java_format.with({ extra_args = { "--aosp" } }),
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-        end,
-      })
+      -- nls.setup({
+      --   sources = {
+      --     -- # FORMATTING #
+      --     fmt.google_java_format.with({ extra_args = { "--aosp" } }),
+      --   },
+      --   on_attach = function(client, bufnr)
+      --     if client.supports_method("textDocument/formatting") then
+      --       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      --       vim.api.nvim_create_autocmd("BufWritePre", {
+      --         group = augroup,
+      --         buffer = bufnr,
+      --         callback = function()
+      --           vim.lsp.buf.format({ bufnr = bufnr })
+      --         end,
+      --       })
+      --     end
+      --   end,
+      -- })
     end,
   },
   {
