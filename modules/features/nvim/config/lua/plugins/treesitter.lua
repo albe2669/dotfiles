c9f3da7b1dee62967e27d9e5c049bdd4e0c88ffa
albe2669 <albert@risenielsen.dk>
2026-07-26 21:41:29 +0200

    fix(treesitter): fix errors

--- modules/features/nvim/config/lua/plugins/treesitter.lua
+++ modules/features/nvim/config/lua/plugins/treesitter.lua
@@ -1,59 +1,55 @@
-local opts = {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = false,
-    disable = {},
-  },
-  auto_paris = { enable = true },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = true,
-  },
-  ensure_installed = {
-    "bash",
-    "c",
-    "c_sharp",
-    "clojure",
-    "cooklang",
-    "dockerfile",
-    "go",
-    "gomod",
-    "graphql",
-    "hcl",
-    "html",
-    "http",
-    "java",
-    "javascript",
-    "jsdoc",
-    "json",
-    "latex",
-    "lua",
-    "markdown",
-    "rust",
-    "scala",
-    "scss",
-    "terraform",
-    "toml",
-    "tsx",
-    "typescript",
-    "yaml",
-    "vim",
-    "vue",
-    "yuck",
-  },
+-- nvim-treesitter's `main` branch is a full rewrite (requires Neovim 0.12+):
+-- highlighting/indent are no longer plugin "modules", they're enabled directly
+-- via core APIs. See https://github.com/nvim-treesitter/nvim-treesitter#readme
+local ensure_installed = {
+  "bash",
+  "c",
+  "c_sharp",
+  "clojure",
+  "cooklang",
+  "dockerfile",
+  "go",
+  "gomod",
+  "graphql",
+  "hcl",
+  "html",
+  "http",
+  "java",
+  "javascript",
+  "jsdoc",
+  "json",
+  "latex",
+  "lua",
+  "markdown",
+  "rust",
+  "scala",
+  "scss",
+  "terraform",
+  "toml",
+  "tsx",
+  "typescript",
+  "yaml",
+  "vim",
+  "vue",
+  "yuck",
 }
 
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter")
+      local ts = require("nvim-treesitter")
 
-      config.setup(opts)
+      ts.setup({})
+      ts.install(ensure_installed)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
     end,
   },
 }
