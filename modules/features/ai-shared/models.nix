5244c05f107e3c8309525047c8a0490deaf033a7
albe2669 <albert@risenielsen.dk>
2026-08-14 15:15:07 +0200

    feat(omp,ai): disable ultra

--- modules/features/ai-shared/models.nix
+++ modules/features/ai-shared/models.nix
@@ -52,22 +52,22 @@
         cacheWrite = 0;
       };
     }
-    {
-      id = "corti-s1-ultra-beta";
-      name = "Corti S1 Ultra Beta (Kimi-K3)";
-      reasoning = true;
-      input = ["text" "image"];
-      contextWindow = 262144;
-      cost = {
-        input = 0;
-        output = 0;
-        cacheRead = 0;
-        cacheWrite = 0;
-      };
-      thinking = {
-        mode = "effort";
-        efforts = ["low" "high" "max"];
-      };
-    }
+    # {
+    #   id = "corti-s1-ultra-beta";
+    #   name = "Corti S1 Ultra Beta (Kimi-K3)";
+    #   reasoning = true;
+    #   input = ["text" "image"];
+    #   contextWindow = 262144;
+    #   cost = {
+    #     input = 0;
+    #     output = 0;
+    #     cacheRead = 0;
+    #     cacheWrite = 0;
+    #   };
+    #   thinking = {
+    #     mode = "effort";
+    #     efforts = ["low" "high" "max"];
+    #   };
+    # }
   ];
 }
