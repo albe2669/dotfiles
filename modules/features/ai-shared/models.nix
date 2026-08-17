101803c4bb1109d1d413e2f68a8aa377df44e120
albe2669 <albert@risenielsen.dk>
2026-08-17 15:34:42 +0200

    fix(ai): models

--- modules/features/ai-shared/models.nix
+++ modules/features/ai-shared/models.nix
@@ -52,22 +52,44 @@
         cacheWrite = 0;
       };
     }
-    # {
-    #   id = "corti-s1-ultra-beta";
-    #   name = "Corti S1 Ultra Beta (Kimi-K3)";
-    #   reasoning = true;
-    #   input = ["text" "image"];
-    #   contextWindow = 262144;
-    #   cost = {
-    #     input = 0;
-    #     output = 0;
-    #     cacheRead = 0;
-    #     cacheWrite = 0;
-    #   };
-    #   thinking = {
-    #     mode = "effort";
-    #     efforts = ["low" "high" "max"];
-    #   };
-    # }
+    {
+      id = "corti-s1-tiny";
+      name = "Corti S1 Tiny";
+      reasoning = true;
+      input = ["text"];
+      contextWindow = 32768;
+      cost = {
+        input = 1;
+        output = 4;
+        cacheRead = 0.2;
+        cacheWrite = 0;
+      };
+    }
+    {
+      id = "corti-s1-tiny-instant";
+      name = "Corti S1 Tiny Instant";
+      reasoning = false;
+      input = ["text"];
+      contextWindow = 32768;
+      cost = {
+        input = 1;
+        output = 4;
+        cacheRead = 0.2;
+        cacheWrite = 0;
+      };
+    }
+    {
+      id = "corti-s1-embedding";
+      name = "Corti S1 Embedding";
+      reasoning = false;
+      input = ["text"];
+      contextWindow = 16384;
+      cost = {
+        input = 0.03;
+        output = 0;
+        cacheRead = 0;
+        cacheWrite = 0;
+      };
+    }
   ];
 }
