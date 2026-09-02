4fe0a69d82af3f2d5cbe96430229dd1ed1f844cb
albe2669 <albert@risenielsen.dk>
2026-09-02 20:17:10 +0200

    fix(golangci-lint): stop overriding

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
 {inputs, ...}: [
   (import ./basedpyright.nix)
   # (import ./claude.nix)
-  (import ./golangci-lint.nix)
+  # (import ./golangci-lint.nix)
   # (import ./jetbrains.nix)
   inputs.claude-code.overlays.default
 ]
